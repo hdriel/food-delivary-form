737a832b823ee7aa555358af487735f03fc31d42
Hadriel Benjo <hdriel@gmail.com>
2025-03-19 13:09:16 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import FoodDeliveryForm from "./FoodDeliveryForm.tsx";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="mx-5">
+          <FoodDeliveryForm />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
